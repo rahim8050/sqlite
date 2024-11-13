d7bbe2cc83ead05579a1315059586a52cd9883cf
rahim8050 <rahimranxx8050@gmail.com>
2024-11-13 07:20:51 +0000

    django

--- sacco/models.py
+++ sacco/models.py
@@ -7,6 +7,22 @@ class customer(models.Model):
     email = models.EmailField(unique=True)
     dob = models.DateField()
     gender = models.CharField(max_length=50)
+    weight = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        db_table = 'customers'
+    class deposit(models.Model):
+        amount = models.IntegerField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+        class Meta:
+            db_table = 'deposits'
 # python  manage.py makemigrations
 # python manage.py migrate
--- sacco/views.py
+++ sacco/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from sacco.models import customer
+
+
 # Create your views here.
+def test(request):
+    # c1 = customer(first_name='John', last_name='Smith', email='john@gmail.com', dob='2006-07-12', gender='Male',weight=45)
+    # c1.save()
+    # c2 = customer(first_name='Joyce',last_name='millie',email='joyce@gmail.com', dob='2007-07-23',gender='female',weight=45)
+    # c2.save()
+    c1= customer.objects.get(id=1)
+    print(c1)
+    d1= deposit(amount=1000,customer=c1)
+    d1.save()
+    customers = customer.objects.count()
+    return HttpResponse(f'ok done we have {customers} customers')
